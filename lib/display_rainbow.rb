3a2ae02b94f1e0ba541fa5874110687c83627ac6
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-18 23:55:59 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,6 @@
 # Write your #display_rainbow method here
 def isplay_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-  
+end 
+
+def
+  puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
