6998a65b8313fbe13854a422d0609beff83a160f
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-19 00:01:35 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,2 +1,8 @@
 display_rainbow = ["R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"]
   
+def display_rainbow(colors)
+end
+
+def display_rainbow(colors)
+  puts "R: #{color[0]}, O: #{color[1]}, Y: #{color[2]}, G: #{color[3]}, B: #{color[4]}, I: #{color[5]}, V: #{color[6]}"
+end
